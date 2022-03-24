44c7c7c39cbae389b3a1feee18b8f9f4becb7b3e
lewiswolf <lewiswolstanholme@gmail.com>
2022-03-24 16:04:34 +0000

    added Timer()

---
+++ test/src/timer.hpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+#include <string>
+
+class Timer {
+   public:
+	Timer() { start_tp = std::chrono::high_resolution_clock::now(); };
+	Timer(std::string s) {
+		name = s;
+		start_tp = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer() { stop(); };
+
+	void stop() {
+		auto end_tp = std::chrono::high_resolution_clock::now();
+		auto start =
+			std::chrono::time_point_cast<std::chrono::microseconds>(start_tp)
+				.time_since_epoch()
+				.count();
+		auto end =
+			std::chrono::time_point_cast<std::chrono::microseconds>(end_tp)
+				.time_since_epoch()
+				.count();
+		std::cout << (name != "" ? name + ": " : "") << end - start << "us\n";
+	};
+
+   private:
+	std::string name = "";
+	std::chrono::time_point<std::chrono::high_resolution_clock> start_tp;
+};
--- test/standalone.cpp
+++ test/standalone.cpp
@@ -1,11 +1,13 @@
 // core
+#include <iostream>	 // cout, endl
 #include <stdlib.h>	 // srand
 #include <time.h>	 // time
 
-#include <iostream>	 // cout, endl
-
 // src
 #include "../src/geometry.hpp"
+
+// test
+#include "./src/timer.hpp"
 
 int main() {
 	// init random number generator
@@ -13,14 +15,27 @@ int main() {
 
 	std::cout << "Test generateConvexPolygon: ";
 	Vertices v = generateConvexPolygon(7);
+
 	for (int i = 0; i < v.size(); i++) {
 		std::cout << "(" << v[i].x << ", " << v[i].y << ") ";
 	}
-	std::cout << std::endl;
-
 	std::cout << "isConvex: ";
-	std::cout << (isConvex(v) ? "true" : "false");
-	std::cout << std::endl << std::endl;
+	std::cout << (isConvex(v) ? "true" : "false") << "\n\n";
+
+	std::cout << "for 1000 vertices";
+	Vertices test;
+	{
+		Timer timer("generateConvexPolygon");
+		test = generateConvexPolygon(1000);
+	}
+	{
+		Timer timer("isConvex");
+		isConvex(test);
+	}
+
+	// std::cout << "bessel J_0(4): ";
+	// std::cout << bessel(0, 4);
+	// std::cout << std::endl << std::endl;
 
 	return 0;
 }

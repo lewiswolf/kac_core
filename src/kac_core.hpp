3f34e4b215b4f6a843ed3eb0f50fd8611220912f
lewiswolf <lewiswolstanholme@gmail.com>
2022-06-22 17:43:59 +0100

    added raised cosine

--- src/kac_core.hpp
+++ src/kac_core.hpp
@@ -2,6 +2,6 @@
 
 // src
 #include "geometry/__index__.hpp"
-// #include "physics/fdtd/__index__.hpp"
+#include "physics/fdtd/__index__.hpp"
 #include "physics/modes/__index__.hpp"
 #include "types.hpp"
---
+++ src/physics/fdtd/__index__.hpp
@@ -0,0 +1,7 @@
+/*
+Central import for all files in /fdtd.
+*/
+
+#pragma once
+
+#include "raised_cosine.hpp"

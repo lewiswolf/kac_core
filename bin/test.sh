863e0af229b3c371b192bed96dcf92c00aec8121
lewiswolf <lewiswolstanholme@gmail.com>
2023-09-24 18:39:06 +0100

    faster testing script

--- bin/test.sh
+++ bin/test.sh
@@ -7,7 +7,6 @@ fi
 
 # build
 cmake -S . -B build
-cmake --build build -j
 
 # run test
-cmake -E env CTEST_OUTPUT_ON_FAILURE=1 cmake --build build --target test
+cmake -E env CTEST_OUTPUT_ON_FAILURE=1 cmake --build build --target test -j

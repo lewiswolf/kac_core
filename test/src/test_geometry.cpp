603dcbae7515e21668f6acbd777244d7b5e64f88
lewiswolf <lewiswolstanholme@gmail.com>
2024-01-09 14:41:39 +0000

    more test

--- test/src/test_geometry.cpp
+++ test/src/test_geometry.cpp
@@ -47,17 +47,17 @@ int main() {
 	/*
 	Test the properties of generateConvexPolygon.
 	*/
-	// booleanTest("generatedConvexPolygon produces n vertices", P_convex.size() == N);
-	// booleanTest("generatedConvexPolygon is convex", g::isConvex(P_convex));
-	// batchBooleanTest(
-	// 	"generatedConvexPolygon does not produce colinear points",
-	// 	N,
-	// 	[&P_convex, &N](unsigned int n) {
-	// 		return !g::isColinear(
-	// 			P_convex[n > 0 ? n - 1 : N - 1], P_convex[n], P_convex[(n + 1) % N]
-	// 		);
-	// 	}
-	// );
+	booleanTest("generatedConvexPolygon produces n vertices", P_convex.size() == N);
+	booleanTest("generatedConvexPolygon is convex", g::isConvex(P_convex));
+	batchBooleanTest(
+		"generatedConvexPolygon does not produce colinear points",
+		N,
+		[&P_convex, &N](unsigned int n) {
+			return !g::isColinear(
+				P_convex[n > 0 ? n - 1 : N - 1], P_convex[n], P_convex[(n + 1) % N]
+			);
+		}
+	);
 	/*
 	Test that convexity holds for both clockwise and anticlockwise oriented polygons.
 	*/

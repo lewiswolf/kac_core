a7b8ffb26134df3bbb1a6425546f9ebb5693475f
lewiswolf <lewiswolstanholme@gmail.com>
2024-01-11 10:38:24 +0000

    windows please

--- bin/test.sh
+++ bin/test.sh
@@ -7,7 +7,7 @@ fi
 
 # build
 cmake -S . -B build
-cmake --build build --config Release -j
+cmake --build build --config Debug -j
 
 # run test
-ctest --test-dir build --config Release -j --output-on-failure
+ctest --test-dir build --C Debug -j --output-on-failure

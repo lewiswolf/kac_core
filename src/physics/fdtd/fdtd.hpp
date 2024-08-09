7ee4dc0bb7ec30fcadb867ea7bcb08cf451939f8
lewiswolf <lewiswolstanholme@gmail.com>
2024-08-09 11:08:19 +0100

    docs

--- src/physics/fdtd/fdtd.hpp
+++ src/physics/fdtd/fdtd.hpp
@@ -137,7 +137,7 @@ namespace kac_core::physics {
 		input:
 			u_0 = initial fdtd grid at t = -1.
 			u_1 = initial fdtd grid at t = 0.
-			B = B conditions.
+			B = boundary conditions.
 			c_0 = first fdtd coefficient related to the decay term and the courant number.
 			c_1 = second fdtd coefficient related to the decay term and the courant number.
 			c_2 = third fdtd coefficient related to the decay term.

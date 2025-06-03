71454ac9f8dfb63b901ac09eb3756467253a14d2
lewiswolf <lewiswolstanholme@gmail.com>
2025-06-03 20:06:46 +0100

    added boundary conditions to linear cymatics

--- src/physics/modes/linear_modes.hpp
+++ src/physics/modes/linear_modes.hpp
@@ -33,18 +33,44 @@ namespace kac_core::physics {
 		return A;
 	}
 
-	inline T::Matrix_1D linearCymatics(const double& n, const unsigned long& H) {
+	inline T::Matrix_1D linearCymatics(
+		const double& n, const unsigned long& H, const std::pair<bool, bool>& boundary_conditions
+	) {
 		/*
 		Produce the 1D continuos cymatic diagram for a particular mode of a linear domain.
 		input:
 			n = nth modal index
 			H = length of the X axis
+			boundary_conditions = boolean pair indicating the boundary conditions
+				- first = left boundary condition (true = fixed, false = free)
+				- second = right boundary condition (true = fixed, false = free)
 		output:
-			M = cos(nπx/H) ≈ 0
+			M = {
+				sin(nπx/H),			(true, true)
+				cos(nπx/H),			(false, false)
+				sin((n-0.5)πx/H),	(true, false) || (false, true)
+			}
 		*/
 
 		T::Matrix_1D M(H);
-		for (unsigned long x = 0; x < H; x++) { M[x] = cos(n * pi * x / H); }
+		double omega = 0.0;
+		if (boundary_conditions.first) {
+			if (boundary_conditions.second) {	 // fixed left and right boundary condition
+				omega = n * pi / H;
+				for (unsigned long x = 0; x < H; x++) { M[x] = sin(omega * x); }
+			} else {							 // fixed left, free right boundary condition
+				omega = (n - 0.5) * pi / H;
+				for (unsigned long x = 0; x < H; x++) { M[x] = sin(omega * x); }
+			}
+		} else {
+			if (boundary_conditions.second) {	 // fixed right, free left boundary condition
+				omega = (n - 0.5) * pi / H;
+				for (unsigned long x = 0; x < H; x++) { M[x] = sin(omega * (H - 1 - x)); }
+			} else {							 // free left and right boundary condition
+				omega = n * pi / H;
+				for (unsigned long x = 0; x < H; x++) { M[x] = cos(omega * x); }
+			}
+		}
 		return M;
 	}
 
